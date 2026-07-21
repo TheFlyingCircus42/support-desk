e2b888a61b5bd1c153d01851f53c93262a9fd644
Rob Hawkinse <rob.hawkins.89@gmail.com>
2026-07-21 16:24:20 +0100

    move browser router up one level to main instead of app file

--- web/src/App.jsx
+++ web/src/App.jsx
@@ -1,17 +1,15 @@
-import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import { Routes, Route } from 'react-router-dom'
 import Home from './pages/Home.jsx'
 import TicketList from './pages/TicketList.jsx'
 import TicketDetail from './pages/TicketDetail.jsx'
 
 function App() {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/tickets" element={<TicketList />} />
-        <Route path="/tickets/:id" element={<TicketDetail />} />
-      </Routes>
-    </BrowserRouter>
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/tickets" element={<TicketList />} />
+      <Route path="/tickets/:id" element={<TicketDetail />} />
+    </Routes>
   )
 }
 
