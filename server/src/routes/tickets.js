137181ed1905fe97bc385612c1a2b5358617e429
Rob Hawkins <rob.hawkins.89@gmail.com>
2026-08-01 12:28:11 +0100

    Consolidate ticket routes into one file

--- server/src/routes/tickets.js
+++ server/src/routes/tickets.js
@@ -1,11 +1,37 @@
 import { Router } from "express";
-import { listTickets } from "../services/ticketService.js";
+import { listTickets, getTicketById, countOpenTickets } from "../services/ticketService.js";
 
 const router = Router();
 
 router.get("/", async (req, res, next) => {
   try {
     res.json(await listTickets());
+  } catch (err) {
+    next(err);
+  }
+});
+
+// /count and /open must come before /:id below - otherwise :id would match
+// "count"/"open" as an id and swallow these requests.
+router.get("/count", async (req, res, next) => {
+  try {
+    res.json({ count: await countOpenTickets() });
+  } catch (err) {
+    next(err);
+  }
+});
+
+router.get("/open", async (req, res, next) => {
+  try {
+    res.json({ count: await countOpenTickets() });
+  } catch (err) {
+    next(err);
+  }
+});
+
+router.get("/:id", async (req, res, next) => {
+  try {
+    res.json(await getTicketById(req.params.id));
   } catch (err) {
     next(err);
   }
